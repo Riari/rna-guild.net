995b62bc06a60e0c9464b03a15fe98e7a72f89ed
Riari <hello@ricko.me>
2016-05-17 21:49:44 +0100

    Add GA tracking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -117,8 +117,15 @@
             $(this).parent('li').addClass('active');
         }
     });
-    </script>
 
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-77914915-1', 'auto');
+    ga('send', 'pageview');
+    </script>
     @yield('bottom')
 </body>
 </html>
