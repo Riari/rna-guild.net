c1331a40f70dcbfd344cd35506d611770867501b
Riari <hello@ricko.me>
2016-06-13 22:04:49 +0100

    Correctly filter threads and posts on homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,13 +32,29 @@ class HomeController extends Controller
 
         $events = Auth::check() ? Event::upcoming() : Event::publicOnly()->upcoming();
 
+        $threads = Thread::with(['author', 'posts'])
+            ->orderBy('created_at', 'desc')
+            ->limit(100)
+            ->get()
+            ->filter(function ($thread) use ($request) {
+                return !$thread->category->private || Auth::check() && $request->user()->can('view', $thread->category);
+            })->take(5);
+
+        $posts = Post::where('post_id', '!=', null)
+            ->orderBy('created_at', 'desc')
+            ->limit(100)
+            ->get()
+            ->filter(function ($post) use ($request) {
+                return !$post->thread->category->private || Auth::check() && $request->user()->can('view', $post->thread->category);
+            })->take(5);
+
         return view('page.home', [
             'quote' => $quotes[rand(0, count($quotes) - 1)],
             'upcomingEvents' => $events->orderBy('ends_at', 'desc')->limit(5)->get(),
             'newUsers' => User::active()->orderBy('created_at', 'desc')->limit(5)->get(),
             'onlineUsers' => Session::authenticated()->groupBy('user_id')->recent()->limit(10)->get(),
-            'newThreads' => Thread::with(['author', 'posts'])->orderBy('created_at', 'desc')->limit(5)->get(),
-            'newPosts' => Post::where('post_id', '!=', null)->orderBy('created_at', 'DESC')->limit(5)->get(),
+            'newThreads' => $threads,
+            'newPosts' => $posts,
             'articles' => Article::published()->orderBy('published_at', 'desc')->paginate()
         ]);
     }
